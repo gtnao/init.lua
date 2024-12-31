360b9af04e1e6b0b9e489b9064e52fef7cd96668
naotaka nakane <n.nakane0219@gmail.com>
2024-12-31 03:13:29 +0000

    Enable priority of colorscheme.

--- init.lua
+++ init.lua
@@ -228,7 +228,7 @@ require("lazy").setup({
 		-- Colorscheme
 		{
 			"ellisonleao/gruvbox.nvim",
-			-- priority = 1000,
+			priority = 1000,
 			config = function()
 				vim.o.background = "dark"
 				vim.cmd([[colorscheme gruvbox]])
