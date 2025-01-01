35470cf15fd40aa398c849986b278f27dee55b59
naotaka nakane <n.nakane0219@gmail.com>
2025-01-01 12:52:43 +0000

    Update.

--- init.lua
+++ init.lua
@@ -1,6 +1,21 @@
 -- Options
 vim.opt.tabstop = 2
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.showmatch = true
+
+vim.opt.number = true
+
+vim.opt.scrolloff = 5
+
+vim.opt.virtualedit = "block"
+
+vim.opt.swapfile = false
+
+vim.opt.termguicolors = true
+
 -- Keymaps
 vim.g.mapleader = " "
 vim.keymap.set({ "n", "x" }, "<Leader>", "<Nop>")
@@ -10,6 +25,14 @@ vim.keymap.set({ "n", "x" }, ":", ";")
 
 vim.keymap.set({ "n" }, "<Leader>w", [[<Cmd>update<CR>]], { silent = true })
 vim.keymap.set({ "n" }, "<Leader>q", [[<Cmd>quit<CR>]], { silent = true })
+
+vim.keymap.set({ "n", "x" }, "<Leader>h", "^")
+vim.keymap.set({ "n", "x" }, "<Leader>l", "$")
+
+vim.keymap.set("n", "<ESC><ESC>", [[<Cmd>nohlsearch<CR>]], { silent = true })
+
+vim.keymap.set("n", "ZZ", "<Nop>")
+vim.keymap.set("n", "ZQ", "<Nop>")
 
 -- Plugin Manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -29,16 +52,24 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+	defaults = {
+		lazy = true,
+	},
 	install = { colorscheme = { "habamax" } },
 	checker = { enabled = true },
 	spec = {
 		-- Completion
 		{
 			"hrsh7th/nvim-cmp",
+			event = { "InsertEnter", "CmdlineEnter" },
 			dependencies = {
 				"hrsh7th/cmp-nvim-lsp",
+				"hrsh7th/cmp-path",
+				"hrsh7th/cmp-nvim-lsp-signature-help",
 				"hrsh7th/cmp-nvim-lua",
+				"hrsh7th/cmp-buffer",
 				"hrsh7th/cmp-cmdline",
+				"hrsh7th/cmp-nvim-lsp-document-symbol",
 				"onsails/lspkind-nvim",
 			},
 			config = function()
@@ -52,7 +83,11 @@ require("lazy").setup({
 					}),
 					sources = cmp.config.sources({
 						{ name = "nvim_lsp" },
+						{ name = "path" },
+						{ name = "nvim_lsp_signature_help" },
 						{ name = "nvim_lua" },
+					}, {
+						{ name = "buffer" },
 					}),
 					formatting = {
 						format = lspkind.cmp_format({
@@ -70,6 +105,14 @@ require("lazy").setup({
 					mapping = cmp.mapping.preset.cmdline(),
 					sources = cmp.config.sources({
 						{ name = "cmdline" },
+						{ name = "path" },
+					}),
+				})
+				cmp.setup.cmdline("/", {
+					mapping = cmp.mapping.preset.cmdline(),
+					sources = cmp.config.sources({
+						{ name = "nvim_lsp_document_symbol" },
+						{ name = "buffer" },
 					}),
 				})
 			end,
@@ -77,6 +120,7 @@ require("lazy").setup({
 		-- LSP
 		{
 			"williamboman/mason.nvim",
+			event = { "VeryLazy" },
 			config = true,
 		},
 		{
@@ -85,6 +129,7 @@ require("lazy").setup({
 				"williamboman/mason.nvim",
 				"neovim/nvim-lspconfig",
 			},
+			event = { "BufReadPre", "BufNewfile" },
 			config = function()
 				local mason_lspconfig = require("mason-lspconfig")
 				local lspconfig = require("lspconfig")
@@ -113,17 +158,17 @@ require("lazy").setup({
 			end,
 		},
 		{
-			"nvimtools/none-ls.nvim",
-			dependencies = {
-				"nvim-lua/plenary.nvim",
-			},
-		},
-		{
 			"jay-babu/mason-null-ls.nvim",
 			dependencies = {
 				"williamboman/mason.nvim",
-				"nvimtools/none-ls.nvim",
+				{
+					"nvimtools/none-ls.nvim",
+					dependencies = {
+						"nvim-lua/plenary.nvim",
+					},
+				},
 			},
+			event = { "BufReadPre", "BufNewfile" },
 			config = function()
 				local mason_null_ls = require("mason-null-ls")
 				local null_ls = require("null-ls")
@@ -151,21 +196,53 @@ require("lazy").setup({
 			end,
 		},
 		{
+			"nvimdev/lspsaga.nvim",
+			dependencies = {
+				"nvim-treesitter/nvim-treesitter",
+				"nvim-tree/nvim-web-devicons",
+			},
+			event = { "VeryLazy" },
+			opts = {
+				lightbulb = {
+					enable = false,
+				},
+				symbol_in_winbar = {
+					enable = false,
+				},
+			},
+		},
+		{
 			"j-hui/fidget.nvim",
+			event = { "BufReadPre", "BufNewfile" },
 			config = true,
 		},
 		-- Treesitter
 		{
 			"nvim-treesitter/nvim-treesitter",
 			build = ":TSUpdate",
+			dependencies = {
+				"RRethy/nvim-treesitter-endwise",
+			},
+			event = { "VeryLazy" },
 			config = function()
 				local configs = require("nvim-treesitter.configs")
 				configs.setup({
 					ensure_installed = {
 						"lua",
+						"tsx",
 					},
 					sync_install = false,
 					highlight = {
+						enable = true,
+					},
+					incremental_selection = {
+						enable = true,
+						keymaps = {
+							node_incremental = "<CR>",
+							node_decremental = "<S-CR>",
+						},
+					},
+					endwise = {
 						enable = true,
 					},
 				})
@@ -178,6 +255,7 @@ require("lazy").setup({
 			dependencies = {
 				"nvim-lua/plenary.nvim",
 			},
+			event = { "VimEnter" },
 			config = true,
 		},
 		-- Filer
@@ -189,13 +267,30 @@ require("lazy").setup({
 				"nvim-tree/nvim-web-devicons",
 				"MunifTanjim/nui.nvim",
 			},
-			config = true,
+			event = { "VimEnter" },
+			opts = {
+				close_if_last_window = true,
+				filesystem = {
+					filtered_items = {
+						hide_dotfiles = false,
+						hide_gitignored = false,
+					},
+					follow_current_file = {
+						enabled = true,
+					},
+					group_empty_dirs = true,
+				},
+			},
+			init = function()
+				vim.keymap.set({ "n" }, "<C-n>", [[<Cmd>Neotree toggle reveal<CR>]], { silent = true })
+			end,
 		},
 		-- AI
 		-- Nodejs is required.
 		-- First, execute `Copilot auth`.
 		{
 			"zbirenbaum/copilot.lua",
+			event = { "InsertEnter" },
 			opts = {
 				suggestion = {
 					auto_trigger = true,
@@ -213,10 +308,12 @@ require("lazy").setup({
 				"sindrets/diffview.nvim",
 				"nvim-telescope/telescope.nvim",
 			},
+			event = { "VeryLazy" },
 			config = true,
 		},
 		{
 			"lewis6991/gitsigns.nvim",
+			event = { "VeryLazy" },
 			config = true,
 		},
 		-- Cmdline
@@ -226,11 +323,19 @@ require("lazy").setup({
 				"MunifTanjim/nui.nvim",
 				"rcarriga/nvim-notify",
 			},
+			event = { "VimEnter" },
 			opts = {
 				popupmenu = {
 					backend = "cmp",
 				},
 			},
+		},
+		-- Terminal
+		{
+			"akinsho/toggleterm.nvim",
+			version = "*",
+			event = { "VimEnter" },
+			config = true,
 		},
 		-- View
 		-- Statusline
@@ -239,6 +344,7 @@ require("lazy").setup({
 			dependencies = {
 				"nvim-tree/nvim-web-devicons",
 			},
+			event = { "VimEnter" },
 			config = true,
 		},
 		-- Bufferline
@@ -248,10 +354,12 @@ require("lazy").setup({
 			dependencies = {
 				"nvim-tree/nvim-web-devicons",
 			},
+			event = { "BufReadPre", "BufNewfile" },
 			config = true,
 		},
 		{
 			"petertriho/nvim-scrollbar",
+			event = { "VeryLazy" },
 			config = true,
 		},
 		-- Startup
@@ -260,6 +368,7 @@ require("lazy").setup({
 			dependencies = {
 				"nvim-tree/nvim-web-devicons",
 			},
+			event = { "VimEnter" },
 			config = function()
 				local startify = require("alpha.themes.startify")
 				startify.file_icons.provider = "devicons"
@@ -269,6 +378,7 @@ require("lazy").setup({
 		-- Edit
 		{
 			"gbprod/substitute.nvim",
+			event = { "VeryLazy" },
 			config = function()
 				local substitute = require("substitute")
 				substitute.setup()
@@ -278,32 +388,88 @@ require("lazy").setup({
 		{
 			"kylechui/nvim-surround",
 			version = "*",
+			event = { "VeryLazy" },
 			config = true,
 		},
 		{
 			"windwp/nvim-autopairs",
+			event = { "InsertEnter" },
+			config = true,
+		},
+		{
+			"windwp/nvim-ts-autotag",
+			event = { "InsertEnter" },
 			config = true,
 		},
 		{
 			"monaqa/dial.nvim",
+			event = { "VeryLazy" },
 			init = function()
 				vim.keymap.set({ "n", "x" }, "+", [[<Plug>(dial-increment)]])
 				vim.keymap.set({ "n", "x" }, "-", [[<Plug>(dial-decrement)]])
 			end,
 		},
+		{
+			"Wansmer/treesj",
+			dependencies = {
+				"nvim-treesitter/nvim-treesitter",
+			},
+			event = { "VeryLazy" },
+			config = function()
+				require("treesj").setup({
+					use_default_keymaps = false,
+				})
+				vim.keymap.set({ "n" }, "<Leader>t", require("treesj").toggle)
+			end,
+		},
+		{
+			"numToStr/Comment.nvim",
+			dependencies = {
+				{
+					"JoosepAlviste/nvim-ts-context-commentstring",
+					dependencies = { "nvim-treesitter/nvim-treesitter" },
+					opts = {
+						enable_autocmd = false,
+					},
+				},
+			},
+			event = { "VeryLazy" },
+			config = function()
+				require("Comment").setup({
+					pre_hook = require("ts_context_commentstring.integrations.comment_nvim")
+					.create_pre_hook(),
+				})
+			end,
+		},
 		-- Search
 		{
 			"kevinhwang91/nvim-hlslens",
+			event = { "VeryLazy" },
 			config = true,
+		},
+		-- Move
+		{
+			"phaazon/hop.nvim",
+			branch = "v2",
+			opts = {
+				keys = "etovxqpdygfblzhckisuran",
+			},
+			event = { "VeryLazy" },
+			init = function()
+				vim.keymap.set("", "fw", [[<Cmd>lua require('hop').hint_words()<CR>]],
+					{ silent = true, remap = true })
+			end,
 		},
 		-- Highlight
 		{
 			"lukas-reineke/indent-blankline.nvim",
 			main = "ibl",
+			event = { "VeryLazy" },
 			config = true,
 		},
 		{
 			"machakann/vim-highlightedyank",
+			event = { "TextYankPost" },
 			init = function()
 				vim.g.highlightedyank_highlight_duration = 300
 			end,
@@ -311,6 +477,7 @@ require("lazy").setup({
 		{
 			"mvllow/modes.nvim",
 			tag = "v0.2.1",
+			event = { "VeryLazy" },
 			config = true,
 		},
 		{
@@ -318,31 +485,52 @@ require("lazy").setup({
 			dependencies = {
 				"nvim-lua/plenary.nvim",
 			},
+			event = { "VeryLazy" },
 			config = true,
 		},
 		{
 			"norcalli/nvim-colorizer.lua",
+			event = { "VeryLazy" },
 			config = true,
 		},
 		-- Quickfix
 		{
 			"kevinhwang91/nvim-bqf",
+			ft = { "qf" },
 			config = true,
+		},
+		-- Session
+		{
+			"farmergreg/vim-lastplace",
+			event = { "BufReadPre" },
 		},
 		-- Notification
 		{
 			"rcarriga/nvim-notify",
+			event = { "VeryLazy" },
 			config = function()
 				vim.notify = require("notify")
+				vim.keymap.set("n", "<BS>", function()
+					for _, win in ipairs(vim.api.nvim_list_wins()) do
+						local buf = vim.api.nvim_win_get_buf(win)
+						if
+						    vim.fn.bufexists(buf) == 1
+						    and vim.api.nvim_get_option_value("filetype", { buf = buf }) == "notify"
+						then
+							vim.api.nvim_win_close(win, { force = false })
+						end
+					end
+				end, { silent = true })
 			end,
 		},
 		-- Colorscheme
 		{
 			"ellisonleao/gruvbox.nvim",
+			lazy = false,
 			priority = 1000,
 			config = function()
 				vim.o.background = "dark"
-				vim.cmd([[colorscheme gruvbox]])
+				vim.cmd.colorscheme("gruvbox")
 			end,
 		},
 	},
