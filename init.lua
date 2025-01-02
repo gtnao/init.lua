4cbe6154b0ef99c365cbe82bc7f8c42ab79e962c
naotaka nakane <n.nakane0219@gmail.com>
2025-01-02 01:49:47 +0000

    Fix notify warning.

--- init.lua
+++ init.lua
@@ -544,18 +544,21 @@ require("lazy").setup({
 			"rcarriga/nvim-notify",
 			event = { "VeryLazy" },
 			config = function()
-				vim.notify = require("notify")
-				vim.keymap.set("n", "<BS>", function()
-					for _, win in ipairs(vim.api.nvim_list_wins()) do
-						local buf = vim.api.nvim_win_get_buf(win)
-						if
-							vim.fn.bufexists(buf) == 1
-							and vim.api.nvim_get_option_value("filetype", { buf = buf }) == "notify"
-						then
-							vim.api.nvim_win_close(win, { force = false })
-						end
-					end
-				end, { silent = true })
+				local notify = require("notify")
+				notify.setup({
+					-- FIXME: Following warning is happened.
+					-- Highlight group 'NotifyBackground' has no background highlight
+					-- Please provide an RGB hex value or highlight group with a background value for 'background_colour' option.
+					-- This is the colour that will be used for 100% transparency.
+					-- ```lua
+					-- require("notify").setup({
+					--   background_colour = "#000000",
+					-- })
+					-- ```
+					-- Defaulting to #000000
+					background_colour = "#282828",
+				})
+				vim.notify = notify
 			end,
 		},
 		-- Colorscheme
